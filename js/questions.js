268f93f8cdd883c9089133503e8635a1bfc19ad2
mahesh9886 <87769642+Karthikl538538@users.noreply.github.com>
2022-08-28 21:12:01 +0530

    upload

---
+++ js/questions.js
@@ -0,0 +1,40 @@
+let MCQS = [{
+            question: "Which HTML tag is used to define an internal style sheet?",
+    choice1: "<script>",
+    choice2: "<style>",
+    choice3: "<html>",
+    choice4: "<svg>",
+    answer: 1
+},
+        {
+            question: "How do you insert a comment in a CSS file?",
+    choice1: "/*This is Comment*/",
+    choice2: "//This Is Comment",
+    choice3: "<!--- This Is Comment --->",
+    choice4: "//This Is Comment//",
+    answer: 1
+},
+            {
+            question: "How do you insert a comment in a HTML file?",
+    choice1: "/*This is Comment*/",
+    choice2: "//This Is Comment",
+    choice3: "<!--- This Is Comment --->",
+    choice4: "//This Is Comment//",
+    answer: 2
+},
+            {
+               question: "How to write an IF statement in JavaScript?",
+    choice1: "if i==5",
+    choice2: "if(i==5)",
+    choice3: "if(i==5)then",
+    choice4: "if i==5 then",
+    answer: 2
+},
+       {
+               question: "How does a WHILE loop start?",
+    choice1: "while(i <= 0)",
+    choice2: "while(i <= 0 i++)",
+    choice3: "while i <= 0",
+    choice4: "while (i++ i <= 0)",
+    answer: 0
+}];
